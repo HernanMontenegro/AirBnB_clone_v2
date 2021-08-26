dbc62a739cf2aa78056933a80a6d664ba703b814
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-26 22:22:43 +0000

    2?

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import local, put, env, run
 import os
 from datetime import datetime
 
-env.hosts = ['35.227.18.212', '	35.185.5.35']
+env.hosts = ['35.227.18.212']
 
 def do_pack():
     """ Create a compression """
@@ -25,7 +25,7 @@ def do_deploy(archive_path):
     put(archive_path, "/tmp/")
     folder_name = archive_path.split('/')[1].split('.')[0]
     path = "data/web_static/releases/" + folder_name
-    run("mkdir " + path)
+    run("mkdir -p " + path)
     run("tar -xzvf /tmp/" + archive_path.split('/')[1] + " -C " + path)
     run("rm /tmp/" + archive_path.split('/')[1])
     run("rm -rf /data/web_static/current")
