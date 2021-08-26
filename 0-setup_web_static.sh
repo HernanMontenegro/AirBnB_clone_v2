915496f8a1607516aada3527025b08c505034cce
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-26 21:03:37 +0000

    now?

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,13 @@ chown -R "$USER:$USER" /var/www/html
 directories=(/data/ /data/web_static/ /data/web_static/releases/ /data/web_static/releases/test/)
 for i in "${directories[@]}"; do if [[ ! -d "$i" ]]; then mkdir "$i" ; fi; done
 
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "42i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
