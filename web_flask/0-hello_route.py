8eb02b1748bd67f2a4cfbada9b3fd2ecb79a1554
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-09 15:45:05 +0000

    returning instead of printing

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+''' First time with Flask '''
 from flask import Flask
 app = Flask(__name__)
 
-# app.url_map.strict_slashes = False
-# app.run(host='0.0.0.0', port=5000)
+app.url_map.strict_slashes = False
+app.run(host='0.0.0.0', port=5000)
+
 @app.route('/')
 def hello_hbnb():
-    print("Hello HBNB!")
+    return "Hello HBNB!"
