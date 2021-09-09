965d81db9b0eb33ab71ebf311dbabcfec44d0348
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-09 18:59:06 +0000

    0 fixed?

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 ''' First time with Flask '''
 from flask import Flask
-app = Flask(__name__)
 
+
+app = Flask(__name__)
 app.url_map.strict_slashes = False
-if (__name__ == '__main__'):
-    app.run(host='0.0.0.0', port=5000)
 
 @app.route('/')
 def hello_hbnb():
+    ''' Return the desired string '''
     return "Hello HBNB!"
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=5000)
