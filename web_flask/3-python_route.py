ad152f6ce7a7b691d30f4902f54acc6e37ca3675
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-09 19:41:01 +0000

    3 done!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' First time with Flask '''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes=False
+
+@app.route('/')
+def hello_hbnb():
+    ''' Return the desired string '''
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    ''' Return the desired string '''
+    return "HBNB"
+
+@app.route('/c/<text>')
+def C_is_fun(text):
+    ''' Return the desired string '''
+    text.replace('_', ' ')
+    return "C " + text
+
+@app.route('/python/(<text>)')
+def Python_is_fun(text="is cool"):
+    ''' Return the desired string '''
+    text.replace('_', ' ')
+    return "Python " + text
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=5000)
