27ba5e649ee2a9b6a02465ec45a25a6ccb54cd6a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-09 19:58:34 +0000

    4 fix

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+''' First time with Flask '''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes=False
+
+@app.route('/')
+def hello_hbnb():
+    ''' Return the desired string '''
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    ''' Return the desired string '''
+    return "HBNB"
+
+@app.route('/c/<text>')
+def C_is_fun(text):
+    ''' Return the desired string '''
+    text.replace('_', ' ')
+    return "C " + text
+
+@app.route('/python/<text>')
+@app.route('/python')
+def Python_is_fun(text="is cool"):
+    ''' Return the desired string '''
+    text.replace('_', ' ')
+    return "Python " + text
+
+@app.route('/number/<n>')
+def display_if_integer(n):
+    ''' Return the desired string '''
+    if (type(n) == int):
+        return "{:d} is a number".format(n)
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=5000)
