2e2ef4ed91c01f81bea7d33de12e15926f056867
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-09 19:31:28 +0000

    2 fixed

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' First time with Flask '''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes=False
+
+@app.route('/')
+def hello_hbnb():
+    ''' Return the desired string '''
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    ''' Return the desired string '''
+    return "HBNB"
+
+@app.route('/c/<text>')
+def C_is_fun(text):
+    ''' Return the desired string '''
+    text.replace('_', ' ')
+    return text
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=5000)
