dab01cb3c0aecd8f12ae49bae1e60379943084c4
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-12 19:38:36 -0300

    ewe

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,6 +9,53 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.route('/')
+def hello_hbnb():
+    ''' Return the desired string '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    ''' Return the desired string '''
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def C_is_fun(text):
+    ''' Return the desired string '''
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/<text>')
+@app.route('/python')
+def Python_is_fun(text="is cool"):
+    ''' Return the desired string '''
+    text.replace('_', ' ')
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def display_if_integer(n):
+    ''' Return the desired string '''
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def display_if_integer_template(n):
+    ''' Return the desired string '''
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def display_if_integer_template_ans(n):
+    ''' Return the desired string '''
+    ans = "even"
+    if (n % 2 != 0):
+        ans = "odd"
+    return render_template('6-number_odd_or_even.html', n=n, answer=ans)
+
+
 @app.teardown_appcontext
 def closeame():
     '''closeame'''
@@ -20,3 +67,6 @@ def States():
     ''' List of all states '''
     list_states = storage.all(State).values()
     return render_template('7-states_list.html', list_states=list_states)
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=5000)
